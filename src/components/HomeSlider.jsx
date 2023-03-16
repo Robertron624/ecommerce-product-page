42e69e6669d0729b1cc02b7a4483be7e637b8f0a
Roberto Ramirez <robert.624@hotmail.com>
2023-03-16 16:44:29 -0500

    added material ui

--- src/components/HomeSlider.jsx
+++ src/components/HomeSlider.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import Carousel from "react-material-ui-carousel";
 
 const HomeSlider = () => {
-  return (
-    <div>HomeSlider</div>
-  )
-}
+    const images = [
+        "/image-product-1.jpg",
+        "/image-product-2.jpg",
+        "/image-product-3.jpg",
+    ];
 
-export default HomeSlider
+    return (
+        <div className="">
+            <Carousel
+                height={300}
+                className={``}
+                NextIcon={
+                    <Image
+                        width={30}
+                        height={30}
+                        src="/icon-next.svg"
+                        alt="next icon"
+                    ></Image>
+                }
+
+                PrevIcon={
+                    <Image
+                        width={30}
+                        height={30}
+                        src="/icon-previous.svg"
+                        alt="next icon"
+                    />
+                }
+            >
+                {images.map((image, index) => (
+                    <Image
+                        className=""
+                        width={300}
+                        height={400}
+                        src={image}
+                        key={index}
+                        alt={`image-${index}`}
+                    />
+                ))}
+            </Carousel>
+        </div>
+    );
+};
+
+export default HomeSlider;
