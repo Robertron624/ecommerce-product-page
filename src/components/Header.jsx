bcbc8f6b3271664fc5555c1f3aa640ecfcb89322
Roberto Ramirez <robert.624@hotmail.com>
2023-03-26 17:33:18 -0500

    changed menu close icon to svg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -78,15 +78,19 @@ const Header = () => {
                                 width: "60vw",
                             }}
                         >
-                            <div className="close-icon">
-                                <Image
-                                    src="/icon-close.svg"
-                                    alt="close drawer icon"
-                                    width={12}
-                                    height={12}
+                            <div className="close-icon mb-6">
+                                <svg
                                     onClick={toggleDrawer(false)}
-                                    className={`mb-6`}
-                                />
+                                    width="14"
+                                    height="15"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+                                        fill="currentcolor"
+                                        fill-rule="evenodd"
+                                    />
+                                </svg>
                             </div>
                             <MobileMenu menuLinks={menuLinks} />
                         </Box>
