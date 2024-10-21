ff723fcacdda609b139e9677ec3c68ef1e423200
Robertron624 <robert.624@hotmail.com>
2024-10-21 16:21:15 -0500

    refactor: using buttons instead of divs to improve accessibility
    Also some styles have been fixed to simplify the position of header links/buttons and minicart menu modals both in mobile and desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,123 +10,126 @@ import Minicart from "./Minicart";
 const menuLinks = ["colections", "men", "women", "about", "contact"];
 
 const Header = () => {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-    const [isMinicartOpen, setIsMinicartOpen] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isMinicartOpen, setIsMinicartOpen] = useState(false);
 
-    const quantity = useSelector((state) => state.product.quantity);
+  const quantity = useSelector((state) => state.product.quantity);
 
-    const toggleDrawer = (open) => (event) => {
-        if (
-            event.type === "keydown" &&
-            (event.key === "Tab" || event.key === "Shift")
-        ) {
-            return;
-        }
-        //changes the function state according to the value of open
-        setIsDrawerOpen(open);
-    };
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    //changes the function state according to the value of open
+    setIsDrawerOpen(open);
+  };
 
-    const handleMinicart = () => {
-        setIsMinicartOpen((isMinicartOpen) => !isMinicartOpen);
-    };
+  const handleMinicart = () => {
+    console.info("Minicart clicked, either redirect to minicart or open a modal");
+    setIsMinicartOpen((isMinicartOpen) => !isMinicartOpen);
+  };
 
-    return (
-        <header className={`py-5 ${styles.headerOuter}`}>
-            <div className={`inner flex justify-between px-6 ${styles.headerInner}`}>
-                <div className=" gap-3 flex items-center">
-                    <Image
-                        onClick={toggleDrawer(true)}
-                        width={15}
-                        height={20}
-                        src="/icon-menu.svg"
-                        alt="menu trigger icon"
-                        className={styles.menuTrigger}
-                    />
-                    <Image
-                        height={90}
-                        width={100}
-                        alt="/site logo"
-                        src="logo.svg"
-                        className={`md:self-start`}
-                    />
-                    <ul
-                        className={`ml-6 hidden justify-between gap-7 h-full ${styles.desktopLinks}`}
-                    >
-                        {menuLinks.map((menuLink, i) => (
-                            <li key={i} className={`${styles.liHeader} flex`}>
-                                <Link
-                                    className={`capitalize ${styles.desktopLink}`}
-                                    href={`/${menuLink}`}
-                                >
-                                    {menuLink}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                    <Drawer
-                        anchor="left"
-                        open={isDrawerOpen}
-                        onClose={toggleDrawer(false)}
-                        onOpen={toggleDrawer(true)}
-                    >
-                        <Box
-                            sx={{
-                                pl: 3,
-                                pr: 4,
-                                py: 2,
-                                height: 1,
-                                backgroundColor: "#fff",
-                                width: "60vw",
-                            }}
-                        >
-                            <div className="close-icon mb-6">
-                                <svg
-                                    onClick={toggleDrawer(false)}
-                                    width="14"
-                                    height="15"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
-                                        fill="currentcolor"
-                                        fill-rule="evenodd"
-                                    />
-                                </svg>
-                            </div>
-                            <MobileMenu menuLinks={menuLinks} />
-                        </Box>
-                    </Drawer>
-                </div>
-                <div className="right flex md:gap-14 gap-4 relative md:items-start items-center">
-                    <div className={`relative`} >
-                        <Image
-                            width={20}
-                            height={20}
-                            src="/icon-cart.svg"
-                            alt="minicart icon"
-                            onClick={handleMinicart}
-                            className={`cursor-pointer ${styles.minicartIcon}`}
-                        />
-                        {quantity > 0 && (
-                            <span
-                                className={`${styles.minicartBadge} absolute`}
-                            >
-                                {quantity}
-                            </span>
-                        )}
-                    </div>
-                    <Image
-                        height={38}
-                        width={38}
-                        src="/image-avatar.png"
-                        alt="user-avatar"
-                        className={`${styles.userAvatar} cursor-pointer`}
-                    />
-                </div>
-            </div>
-            <Minicart isMinicartOpen={isMinicartOpen} setIsMinicartOpen={setIsMinicartOpen}/>
-        </header>
-    );
+  const handleAvatarClick = () => {
+    console.log("Avatar clicked, either redirect to profile or open a modal");
+  };
+
+  return (
+    <header className={`py-5 relative ${styles.headerOuter}`}>
+      <div className={`inner flex justify-between px-6 ${styles.headerInner}`}>
+        <div className=' gap-3 flex items-center'>
+          <Image
+            onClick={toggleDrawer(true)}
+            width={15}
+            height={20}
+            src='/icon-menu.svg'
+            alt='menu trigger icon'
+            className={styles.menuTrigger}
+          />
+          <Link href='/' className='h-full flex items-center'>
+            <Image height={90} width={100} alt='/site logo' src='logo.svg' />
+          </Link>
+          <ul
+            className={`ml-6 hidden justify-between gap-7 h-full ${styles.desktopLinks}`}
+          >
+            {menuLinks.map((menuLink, i) => (
+              <li key={i} className={`${styles.liHeader} flex`}>
+                <Link
+                  className={`capitalize flex items-center ${styles.desktopLink}`}
+                  href={`/${menuLink}`}
+                >
+                  {menuLink}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <Drawer
+            anchor='left'
+            open={isDrawerOpen}
+            onClose={toggleDrawer(false)}
+            onOpen={toggleDrawer(true)}
+          >
+            <Box
+              sx={{
+                pl: 3,
+                pr: 4,
+                py: 2,
+                height: 1,
+                backgroundColor: "#fff",
+                width: "60vw",
+              }}
+            >
+              <div className='close-icon mb-6'>
+                <svg
+                  onClick={toggleDrawer(false)}
+                  width='14'
+                  height='15'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    d='m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z'
+                    fill='currentcolor'
+                    fill-rule='evenodd'
+                  />
+                </svg>
+              </div>
+              <MobileMenu menuLinks={menuLinks} />
+            </Box>
+          </Drawer>
+        </div>
+        <div className='right flex md:gap-14 gap-4 relative items-center'>
+          <button className={`relative`} onClick={handleMinicart}>
+            <Image
+              width={20}
+              height={20}
+              src='/icon-cart.svg'
+              alt='minicart icon'
+              className={`cursor-pointer ${styles.minicartIcon}`}
+            />
+            {quantity > 0 && (
+              <span className={`${styles.minicartBadge} absolute`}>
+                {quantity}
+              </span>
+            )}
+          </button>
+          <button className='cursor-pointer' onClick={handleAvatarClick}>
+            <Image
+              height={38}
+              width={38}
+              src='/image-avatar.png'
+              alt='user-avatar'
+              className={`${styles.userAvatar} cursor-pointer`}
+            />
+          </button>
+        </div>
+      </div>
+      <Minicart
+        isMinicartOpen={isMinicartOpen}
+        setIsMinicartOpen={setIsMinicartOpen}
+      />
+    </header>
+  );
 };
 
 export default Header;
--- src/components/QuantitySelector.jsx
+++ src/components/QuantitySelector.jsx
@@ -1,29 +1,52 @@
-import Image from 'next/image'
-import React, { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import styles from '@/styles/QuantitySelector.module.css'
-import { addOne, removeOne } from '@/redux/productSlice'
+import Image from "next/image";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import styles from "@/styles/QuantitySelector.module.css";
+import { addOne, removeOne } from "@/redux/productSlice";
 
 const QuantitySelector = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const quantity = useSelector((state) => state.product.quantity);
+  const addOneCall = () => {
+    dispatch(addOne());
+  };
 
-    const quantity = useSelector(state => state.product.quantity)
-    const addOneCall = () => {
-      dispatch(addOne())
-    }
-
-    const removeOneCall = () => {
-      dispatch(removeOne())
-    }
+  const removeOneCall = () => {
+    dispatch(removeOne());
+  };
 
   return (
-    <div className={`mt-5 rounded-md py-3 px-2 w-full flex justify-between items-center ${styles.quantity_container}`}>
-        <Image onClick={removeOneCall} className={`${styles.quantity_minus} cursor-pointer`} width={10} height={1} alt='quantity minus' src="/icon-minus.svg"/>
-        <span className='font-bold'>{quantity}</span>
-        <Image onClick={addOneCall} className={`${styles.quantity_plus} cursor-pointer`} width={10} height={5} src="/icon-plus.svg" alt='quantity plus'/>
+    <div
+      className={`rounded-md w-full flex justify-between items-center ${styles.quantity_container}`}
+    >
+      <button
+        className='cursor-pointer py-[1.188rem] px-2'
+        onClick={removeOneCall}
+      >
+        <Image
+          className={`${styles.quantity_minus}`}
+          width={10}
+          height={1}
+          alt='quantity minus'
+          src='/icon-minus.svg'
+        />
+      </button>
+      <span className='font-bold'>{quantity}</span>
+      <button
+        className='cursor-pointer py-[1.188rem] px-2'
+        onClick={addOneCall}
+      >
+        <Image
+          className={`${styles.quantity_plus}`}
+          width={10}
+          height={5}
+          src='/icon-plus.svg'
+          alt='quantity plus'
+        />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default QuantitySelector
+export default QuantitySelector;
